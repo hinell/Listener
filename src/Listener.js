62f96b4e7cb93dacc95dd5d3f507d1869ade968f
Hinell <al.neodim@gmail.com>
2017-04-09 21:45:01 +0300

    Refine interface

--- src/Listener.js
+++ src/Listener.js
@@ -21,23 +21,25 @@ function Listener(callback) {
         var l = arr.length, arr = [].concat(arr);
         while (l--) {
               arr.each_(function (a,b) {
-                (a.on || a.addEventListener).call(a,eventName, function () {
-                  var args = [].slice.call(arguments);
-                      args.splice(0,0,{origin: a, target: b});
-                      this.apply(b,args); // call Listener right here
-                }.bind(this));
-                (b.on || b.addEventListener).call(b,eventName, function () {
-                  var args = [].slice.call(arguments);
-                      args.splice(0,0,{origin: b, target: a});
-                      this.apply(a,args); // call Listener right here
-                }.bind(this))
+                this.on(a,b,eventName)
               }.bind(this,arr.shift()));
         }
       };
+      //
       listener.on     = function (a,b,eventName) {
         if(!a || !b) { throw 'Invalid arguments: Both event emitters are required!' }
         if(!eventName || !eventName.split ) {throw 'Invalid argument: Event name is missing or is invalid type.'}
-          this.toArr([a,b],eventName);
+          // TODO: Make sure no duplicates being added
+          (a.on || a.addEventListener).call(a,eventName, function () {
+          var args = [].slice.call(arguments);
+              args.splice(0,0,{origin: a, target: b});
+              this.apply(b,args); // call Listener right here
+        }.bind(this));
+          (b.on || b.addEventListener).call(b,eventName, function () {
+          var args = [].slice.call(arguments);
+              args.splice(0,0,{origin: b, target: a});
+              this.apply(a,args); // call Listener right here
+        }.bind(this))
       };
   return listener
 }
